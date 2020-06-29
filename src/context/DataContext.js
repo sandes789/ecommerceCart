36733383098e7322d51ed7fca184b46b19dec914
Prateek Thapa <prateekthapa26@gmail.com>
2020-06-29 14:16:07 +0545

    store the items on localStorage

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -3,6 +3,8 @@ import {v1 as uuid} from 'uuid'
 
 export const DataContext = createContext();
 
+const KEY = 'ECOMMERCE_CART_ITEMS';
+
 const DataContextProvider = (props) => {
 
     const [items, setItems] = useState([
@@ -11,9 +13,14 @@ const DataContextProvider = (props) => {
         {name:'Guitar',price: 300, qty: 1, id:3 },
     ])
 
-    // useEffect(() => {
-    //     localStorage.setItem('items',JSON.stringify(items))
-    // },[items])
+    useEffect(() => {
+        localStorage.setItem(KEY,JSON.stringify(items))
+    },[items])
+
+    useEffect(() => {
+        const persistedItems = localStorage.getItem(KEY);
+        if (persistedItems) setItems(JSON.parse(persistedItems));
+    }, []);
 
       
     const addItems = (name,price,qty) => {
